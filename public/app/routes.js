5f209d751fc4de503217aa332bbf8276e6caaf29
StoyanTakov <p3rf0rat0ra@gmail.com>
2017-10-29 20:17:56 +0200

    Added back-end corrections, public files, angular, bootstrap, jquery

---
+++ public/app/routes.js
@@ -0,0 +1,19 @@
+angular.module('appRoutes', ['ngRoute'])
+
+    .config(function ($routeProvider,$locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'app/views/pages/home-page.html'
+            })
+            .when('/video', {
+                templateUrl: 'app/views/pages/video-page.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    });
