0b990f9d19cec46e8e57c26089c38e06cec691a3
StoyanTakov <p3rf0rat0ra@gmail.com>
2017-10-31 20:34:02 +0200

    corrected link ref

--- app/routes/api.js
+++ app/routes/api.js
@@ -2,7 +2,7 @@ var User = require("../models/user")
 
 module.exports = function (router) {
 
-    // http://localhost:8080/users
+    // http://localhost:8080/api/users
     router.post('/users', function (req, res) {
         var user = new User();
         user.username = req.body.username;
