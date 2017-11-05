0a4d297001b690cb34f862535c8322ee0bec8e3c
metodiMatov <metodi.matov@gmail.com>
2017-11-05 07:39:02 +0200

    video model

---
+++ app/models/video.js
@@ -0,0 +1,61 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var VideoSchema = new Schema({
+    file: {
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    publisher: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    publishInfo: {
+        time: {
+            type: String,
+            required: true
+        },
+        categories:{
+            type: String,
+            required: true
+        },
+        tags:{
+            type: String,
+            required: true,
+        },
+        description:{
+            type: String,
+            required: true
+        },
+        views:{
+            type: String,
+            required: true
+        },
+        likes:{
+            type: String,
+            required: true
+        },
+        dislikes:{
+            type: String,
+            required: true
+        },
+
+    },
+    comments:{
+        type: String,
+        required: true
+    }
+});
+
+
+
+module.exports = mongoose.model("Video", VideoSchema);
