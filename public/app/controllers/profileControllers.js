f485a081a401f560893652f678f1491a422cc56f
metodiMatov <metodi.matov@gmail.com>
2017-11-06 19:03:15 +0200

    user profile bar

--- public/app/controllers/profileControllers.js
+++ public/app/controllers/profileControllers.js
@@ -1,31 +1,31 @@
 angular.module('profileControllers', [])
     .controller('profileBarController', function ($scope) {
 
-        $scope.showHome = true;
+        $scope.showHome = false;
         $scope.hideHome = hideHome;
         function hideHome() {
-            if ($scope.showHome == true) {
-                $scope.showHome = false;
-            } else {
+            if ($scope.showHome == false) {
                 $scope.showHome = true;
+            } else {
+                $scope.showHome = false;
             }
         }
         $scope.showVideos = true;
         $scope.showAllVideos = showAllVideos;
         function showAllVideos() {
             if ($scope.showVideos == true) {
-                $scope.showVideos == false;
+                $scope.showVideos = false;
             } else {
-                $scope.showVideos == true;
+                $scope.showVideos = true;
             }
         }
         $scope.showPlaylist = true;
         $scope.showPlaylists = showPlaylists;
         function showPlaylists() {
             if ($scope.showPlaylist == true) {
-                $scope.showPlaylist == false;
+                $scope.showPlaylist = false;
             } else {
-                $scope.showPlaylist == true;
+                $scope.showPlaylist = true;
             }
 
         }
